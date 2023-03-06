9da6cd220c550419321f1182c8af478236b43c4e
Angel Eduardo <aepatino.360@gmail.com>
2023-03-06 00:41:03 -0400

    Add Lesson of Classes & Instances of OOP & Algorithms Course.

--- Curso de POO y Algoritmos en Python/classes.py
+++ Curso de POO y Algoritmos en Python/classes.py
@@ -10,29 +10,36 @@ class Human:
         self._presupuesto = 0
 
     def greetings(self, other_person):
-        return f"Hello {other_person}, my name is {self.nombre}"
+        return f"Hello {other_person.nombre}, my name is {self.nombre}"
     
     def profession_explanation(self, other_person):
         return f"{other_person.nombre}, yo trabajo como un {self.profesion}"
     
     def _define_budget(self, person_budget):
         self._presupuesto = person_budget
-        print(self._presupuesto)
     
 
 
 if __name__ == '__main__':
     
-    name, age, profession, country = input("Ingresa tu nombre, edad, profesion y pais: ")
+    name, age, profession, country = input("Ingresa tu nombre, edad, profesion y pais: ").split()
     age = int(age)
 
     first_person = Human(name, age, profession, country)
     
-    name, age, profession, country = input("Ingresa el nombre, edad, profesion y pais de la otra persona: ")
+    name, age, profession, country = input("Ingresa el nombre, edad, profesion y pais de la otra persona: ").split()
     age = int(age)
 
     second_person = Human(name, age, profession, country)
 
-    budget, other_budget = int(input("Ingresa el presupuesto de ambas personas por separado: "))
-    first_person._define_budget(budget)
-    second_person._define_budget(other_budget)
+    budget, other_budget = input("Ingresa el presupuesto de ambas personas por separado: ").split()
+    first_person._define_budget(int(budget))
+    second_person._define_budget(int(other_budget))
+
+    greet = first_person.greetings(second_person)
+    print(greet)
+
+    profession_explain = first_person.profession_explanation(second_person)
+    print(profession_explain)
+    
+    print(f"El presupuesto de {first_person.nombre} es de {first_person._presupuesto}, mientras que el presupuesto de {second_person.nombre} es de {second_person._presupuesto}")
